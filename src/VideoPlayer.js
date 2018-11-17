e37bb7c8bca69301b1461fbff0ac1d99840f1f71
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2018-11-16 23:22:21 -0600

    last changes

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player'
 
 class VideoPlayer extends Component {
   render () {
-    return <ReactPlayer url='https://www.youtube.com/watch?v=mLRjb6LdUFM' playing />
+    return <ReactPlayer url='https://www.youtube.com/watch?v=mLRjb6LdUFM'  />
   }
 }
 
