862c939e076e30bfdbe3be2c330a2815a084539e
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2018-11-18 18:38:00 -0600

    added references

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App extends Component {
     return (
       <div className="App">
         <ul id="ul">
-          <li><a href="#a">Introduction</a></li>
-          <li><a href="#b">Methane Gas</a></li>
-          <li><a href="#c">Hunger</a></li>
-          <li><a href="#d">Reducing Food Waste</a></li>
+          <li><a href="#food-waste">Introduction</a></li>
+          <li><a href="#methane-gas">Methane Gas</a></li>
+          <li><a href="#hunger-in-america">Hunger</a></li>
+          <li><a href="#reducing-food-waste">Reduction</a></li>
+          <li><a href="#references">References</a></li>
         </ul>
         <div id="container">
-          <div className="one" id="a">
+          <div className="one" id="food-waste">
             <header className="App-header">
               <img src={require('./images/gfx-food-waste.png')} id="headerImg" alt="recycle"/>
               <h1>Food Waste</h1>
@@ -25,7 +26,7 @@ class App extends Component {
               <Chart/>
             </div>
           </div>
-          <div className="two" id="b">
+          <div className="two" id="methane-gas">
             <h1 align="left" id="methane">Food Waste in Landfills Generates Methane Gas!!!</h1>
             <p align="left" className="intro">Methane is a greenhouse gas. In 2016, methane accounted for <strong>10%</strong> of all U.S. greenhouse gas emissions from human activities. As shown in the image below, a monthly peak of <strong>1887.65 ppb</strong> was reached in November 2017.
             </p>
@@ -34,7 +35,7 @@ class App extends Component {
             <p align="left" className="intro">Methane is a gas with 25 fold more potent global warming potential than carbon di-oxide which would have been the primary end product had the food been eaten and metabolized by humans. </p>
             <p align="left" className="intro"> <strong>For more information:</strong> <a href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0007940" className="hyperlink">The Progressive Increase of Food Waste in America and Its Environmental Impact</a></p>
           </div>
-          <div className="three" id="c">
+          <div className="three" id="hunger-in-america">
             <h1 align="left" id="methane">1 in 6 people in America face hunger!</h1>
             <p align="left" className="intro">Even in the world’s greatest food-producing nation, children and adults face poverty and hunger in every county across America. In 2017:</p>
             <ul className="list">
@@ -44,7 +45,7 @@ class App extends Component {
             </ul>
             <img src={require('./images/nebraska-hunger.PNG')} alt="nebraska-hunger" className="hungerNebraska"/>
           </div>
-          <div className="two"  id="d">
+          <div className="two"  id="reduce-food-waste">
             <h1 align="left" id="methane">How can we reduce Food Waste? <img src={require('./images/adults.png')} alt="people" align="middle" className="lastSection"/> </h1>
             <br/>
             <p align="left" className="intro"> The reduction of food waste starts at home. If you start keeping track of what you put in your body and
@@ -57,6 +58,19 @@ class App extends Component {
             <div className="video">
               <VideoPlayer/>
             </div>
+          </div>
+          <div className="last" id="references">
+            <h1 align="left" id="methane">References</h1>
+            <br/>
+            <p align="left" className="intro">United States Department of Agrilculture, www.usda.gov/oce/foodwaste/faqs.htm.</p>
+            <br/>
+            <p align="left" className="intro">“Facts About Hunger and Poverty in America.” Feeding America, Feeding America, www.feedingamerica.org/hunger-in-america/facts.</p>
+            <br/>
+            <p align="left" className="intro">“Nebraska.” Feeding America, Feeding America, www.feedingamerica.org/hunger-in-america/nebraska.</p>
+            <br/>
+            <p align="left" className="intro">Hall KD, Guo J, Dore M, Chow CC (2009) The Progressive Increase of Food Waste in America and Its Environmental Impact. PLOS ONE 4(11): e7940. https://doi.org/10.1371/journal.pone.0007940</p>
+            <br/>
+            <p align="left" className="intro">“Overview of Greenhouse Gases.” EPA, Environmental Protection Agency, 31 Oct. 2018, www.epa.gov/ghgemissions/overview-greenhouse-gases.</p>
           </div>
         </div>
       </div>
